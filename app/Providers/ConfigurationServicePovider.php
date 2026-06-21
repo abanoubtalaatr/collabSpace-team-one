12ba20088e658c9d4c2b83e82404d581ac41f42f
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-06-21 23:30:58 +0300

    feat: Add ConfigurationServiceProvider to manage application configurations

---
+++ app/Providers/ConfigurationServicePovider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Admin;
+use App\Models\Banner;
+use App\Models\Block;
+use App\Models\City;
+use App\Models\Compound;
+use App\Models\Unit;
+use App\Models\UnitTransaction;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class ConfigurationServicePovider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrl();
+        $this->relationEnforceMorphMap();
+    }
+
+    private function configureCommands()
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    private function configureModels()
+    {
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
+    }
+
+    private function configureUrl()
+    {
+        URL::forceHttps(app()->isProduction());
+    }
+
+    private function relationEnforceMorphMap()
+    {
+        Relation::enforceMorphMap([
+            'user' => User::class,
+            'admin' => Admin::class,
+            'compound' => Compound::class,
+            'block' => Block::class,
+            'unit' => Unit::class,
+            'city' => City::class,
+            'banner' => Banner::class,
+            'unit_transaction' => UnitTransaction::class
+        ]);
+    }
+}
