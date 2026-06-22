31e4cf9e82dc1f959761c7ff30c2637f3158315a
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-06-22 22:57:06 +0300

    feat: add migration models and relationships

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -7,4 +7,9 @@ use Laratrust\Models\Team as LaratrustTeam;
 class Team extends LaratrustTeam
 {
     public $guarded = [];
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class, 'project_team');
+    }
 }
