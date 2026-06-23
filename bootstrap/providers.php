90a4e21fba79f74513145046b9babd2dc0998b42
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-06-23 13:59:30 +0300

    refactor: api response macros to trait

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,8 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\ConfigurationServicePovider;
-use App\Providers\ResponseServiceProvider;
 
 return [
     AppServiceProvider::class,
     ConfigurationServicePovider::class,
-    ResponseServiceProvider::class
 ];
