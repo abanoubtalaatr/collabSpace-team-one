f3f92ef8c2d97e1cd7bb38811ea1cffe6903f590
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2026-06-25 22:17:05 +0300

    wip

--- routes/team.php
+++ routes/team.php
@@ -5,11 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 // , 'middleware' => ['role:admin']
 Route::group(['prefix' => 'teams'], function () {
-    Route::get('/', [TeamsController::class, 'index'])->name('teams.index');
-    Route::get('/create', [TeamsController::class, 'create'])->name('teams.create');
-    Route::post('/store', [TeamsController::class, 'store'])->name('teams.store');
-    Route::get('/{teamId}', [TeamsController::class, 'show'])->name('teams.show');
-    Route::get('/{teamId}/edit', [TeamsController::class, 'edit'])->name('teams.edit');
-    Route::put('/{teamId}/update', [TeamsController::class, 'update'])->name('teams.update');
-    Route::delete('/{teamId}/delete', [TeamsController::class, 'delete'])->name('teams.delete');
+Route::apiResource('teams', TeamsController::class);
 });
+
