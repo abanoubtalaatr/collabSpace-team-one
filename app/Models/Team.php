b335274a1a946e61e360098f4f429023382a218c
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2026-06-25 21:02:50 +0300

    wip

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -9,7 +9,9 @@ use Laratrust\Models\Team as LaratrustTeam;
 
 class Team extends LaratrustTeam
 {
-    public $guarded = [];
+    public $fillable = [
+        'name'
+    ];
 
     public function projects(): BelongsToMany
     {
