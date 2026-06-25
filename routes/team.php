01abf566aba10ed215a7346b5807cd85fcc6bb49
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2026-06-25 22:17:20 +0300

    wip

--- routes/team.php
+++ routes/team.php
@@ -4,7 +4,5 @@ use App\Http\Controllers\teams\TeamsController;
 use Illuminate\Support\Facades\Route;
 
 // , 'middleware' => ['role:admin']
-Route::group(['prefix' => 'teams'], function () {
 Route::apiResource('teams', TeamsController::class);
-});
 
